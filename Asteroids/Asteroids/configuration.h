d9de341de1b513d3c8504abc4d403e07e4bafa23
loganofcarpenter <loganofcarpenter@gmail.com>
2015-12-09 18:05:49 -0700

    Create configuration.h
    File made for keeping all the constants that configure the game.

---
+++ Asteroids/Asteroids/configuration.h
@@ -0,0 +1,15 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#define WINDOW_X_SIZE 200
+#define WINDOW_Y_SIZE 200
+
+
+float xMax = WINDOW_X_SIZE;
+float xMin = -WINDOW_X_SIZE;
+float yMax = WINDOW_Y_SIZE;
+float yMin = -WINDOW_Y_SIZE;
+
+
+#endif
+
